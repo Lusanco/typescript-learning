1e7d149d072ab03ad95cb87a6dcc6072c199d06a
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-10-23 11:56:00 -0400

    update - error handling with interfaces

--- 00-a-first-app/index.ts
+++ 00-a-first-app/index.ts
@@ -2,6 +2,21 @@ import axios from "axios";
 
 const url: string = "https://jsonplaceholder.typicode.com/todos/1";
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: Boolean;
+}
+
 axios.get(url).then((response) => {
-  console.log(response.data);
+  const todo: Todo = response.data;
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  console.log(`
+    The Todo with id: ${id}
+    Has a title of: ${title}
+    Is it completed? ${completed}
+    `);
 });
