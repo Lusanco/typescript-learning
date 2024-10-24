e7cfac31a2d4eab520cb3460dc81e801815eea00
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-10-24 00:50:46 -0400

    update - when we declare a variable on one line and initialize later

--- 01-examples-of-types/annotations/variables.ts
+++ 01-examples-of-types/annotations/variables.ts
@@ -33,3 +33,11 @@ const logNumber: (i: number) => void = (i: number) => {
 const json = '{"x": 10, "y": 20}';
 const coordinates: { x: number; y: number } = JSON.parse(json);
 console.log(coordinates);
+
+// 2) When we declare a variable on one line and initialize later
+let words = ["red", "green", "blue"];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  foundWord = true;
+}
