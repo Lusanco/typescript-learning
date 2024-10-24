23100c375796b31106fa7343224018eae049277c
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-10-24 00:56:24 -0400

    update - variable whose type cannot be inferred correctly

--- 01-examples-of-types/annotations/variables.ts
+++ 01-examples-of-types/annotations/variables.ts
@@ -41,3 +41,13 @@ let foundWord: boolean;
 for (let i = 0; i < words.length; i++) {
   foundWord = true;
 }
+
+// 3) Variable whose type cannot be inferred correctly (bad code example)
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
