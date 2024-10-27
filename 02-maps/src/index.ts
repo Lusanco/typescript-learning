f96b7806dec879be9963dc30453c7e00a3db7da4
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-10-27 15:59:57 -0400

    checkpoint - work in progress

--- 02-maps/src/index.ts
+++ 02-maps/src/index.ts
@@ -1,8 +1,5 @@
-import { User } from "./User";
-import { Company } from "./Company";
+/// <reference types="@types/google.maps" />
+// import { User } from "./User";
+// import { Company } from "./Company";
 
-const user = new User();
-const company = new Company();
-
-console.log(user);
-console.log(company);
+new google.maps.Map(document.getElementById("map") as HTMLElement, {});
