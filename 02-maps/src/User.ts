d506d0d959b8cc93ac9cc7d35b834f2d9318f730
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-10-29 10:53:19 -0400

    implements Mappable help typescript for errors

--- 02-maps/src/User.ts
+++ 02-maps/src/User.ts
@@ -1,19 +1,23 @@
 import { faker } from "@faker-js/faker";
-
+import { Mappable } from "./CustomMap";
 // convention in typescript not to use default keyword to avoid typos and confusion
 // export default "red";
 
-export class User {
+export class User implements Mappable {
   name: string;
   location: {
     lat: number;
     lng: number;
   };
+  color: string = "red";
   constructor() {
     this.name = faker.name.firstName("male");
     this.location = {
       lat: parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude()),
     };
+  }
+  markerContent(): string {
+    return `User Name: ${this.name}`;
   }
 }
