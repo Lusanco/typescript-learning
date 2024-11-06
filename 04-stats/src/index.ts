6706de6d50547fc6997fc7131807697b6c5b0ca6
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-11-06 11:25:15 -0400

    refactoring with static methods

--- 04-stats/src/index.ts
+++ 04-stats/src/index.ts
@@ -1,18 +1,8 @@
-import { HTMLReport } from "./reportTargets/HTMLReport";
-import { ConsoleRport } from "./reportTargets/ConsoleReport";
-import { CSVFileReader } from "./CSVFileReader";
 import { MatchReader } from "./MatchReader";
-import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 import { Summary } from "./Summary";
 
-// Create object that satisfies the DataReader Interface
-const csvFileReader = new CSVFileReader("football.csv");
+const matchReader = MatchReader.fromCSV("football.csv");
+const summary = Summary.winsAnalysisWithHTMLReport("Man United");
 
-// Create instance of MatchReader and pass in
-// something satisfying DataReader interface
-const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
-
-const summary = new Summary(new WinsAnalysis("Man United"), new HTMLReport());
-
 summary.buildAndPrintReport(matchReader.matches);
