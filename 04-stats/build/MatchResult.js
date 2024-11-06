baa9bc394a44403433000740baf0cbfddf931bd2
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-11-06 10:59:23 -0400

    fix - builds included by mistake

--- 04-stats/build/MatchResult.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MatchfResult = void 0;
-var MatchfResult;
-(function (MatchfResult) {
-    MatchfResult["HomeWin"] = "H";
-    MatchfResult["AwayWin"] = "A";
-    MatchfResult["Draw"] = "D";
-})(MatchfResult || (exports.MatchfResult = MatchfResult = {}));
