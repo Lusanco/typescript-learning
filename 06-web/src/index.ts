9582ca01eebc85d3f05a3d8a4d481c5c254b7885
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-11-08 12:01:15 -0400

    triggering event callbacks

--- 06-web/src/index.ts
+++ 06-web/src/index.ts
@@ -2,8 +2,13 @@ import { User } from "./models/User";
 
 const user = new User({});
 
-user.on("change", () => {});
-user.on("change", () => {});
-user.on("click", () => {});
-
-console.log(user);
+user.on("change", () => {
+  console.log("Change 1");
+});
+user.on("change", () => {
+  console.log("Change 2");
+});
+user.on("click", () => {
+  console.log("Cliked");
+});
+user.trigger("click");
