01085b638a8d0ea78f1524ec8e660f3502ddb676
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-11-08 11:21:00 -0400

    optional properties

--- 06-web/src/index.ts
+++ 06-web/src/index.ts
@@ -1,6 +1,10 @@
 import { User } from "./models/User";
 
-const user = new User({ name: "myname", age: 20 });
+const user = new User({});
+
+user.set({ name: "NewName", age: 88 });
+
+user.set({ name: "onlyName" });
 
 console.log(user.get("name"));
 console.log(user.get("age"));
