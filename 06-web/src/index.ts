654996aa03fb5047632092abb4ab4f757c909b4d
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-11-08 12:35:53 -0400

    cofigs and npm installs

--- 06-web/src/index.ts
+++ 06-web/src/index.ts
@@ -5,10 +5,13 @@ const user = new User({});
 user.on("change", () => {
   console.log("Change 1");
 });
+
 user.on("change", () => {
   console.log("Change 2");
 });
+
 user.on("click", () => {
   console.log("Cliked");
 });
+
 user.trigger("click");
