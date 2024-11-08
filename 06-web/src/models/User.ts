c93037a70d826f992ab3dd70c95a3c9a80d2cce2
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-11-08 11:12:02 -0400

    initial setup and intro

---
+++ 06-web/src/models/User.ts
@@ -0,0 +1,18 @@
+// his implementation with interfaces
+// interface UserProps {
+//   name: string;
+//   age: number;
+// }
+
+// my try enforcing types
+type UserProps = {
+  name: string;
+  age: number;
+};
+
+export class User {
+  constructor(private data: UserProps) {}
+  get(propName: string): number | string {
+    return this.data[propName];
+  }
+}
