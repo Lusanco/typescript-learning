7d0532517225ab75eee42f71e33d5c4fdeb3fa35
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-11-11 13:51:23 -0400

    Fetching User Data

--- 06-web/src/index.ts
+++ 06-web/src/index.ts
@@ -1,11 +1,9 @@
 import { User } from "./models/User";
 
-const user = new User({ name: "El Power", age: 619 });
-
-console.log(user.get("name"));
+const user = new User({ id: 1 });
 
 user.on("change", () => {
-  console.log("changed!");
+  console.log(user);
 });
 
-user.set({ name: "New Name" });
+user.fetch();
--- 06-web/src/models/User.ts
+++ 06-web/src/models/User.ts
@@ -1,6 +1,7 @@
 import { Eventing } from "./Eventing";
 import { Sync } from "./Sync";
 import { Attributes } from "./Attributes";
+import { AxiosResponse } from "axios";
 
 // interface UserProps { name: string; age: number; }
 // using type instead of interface
@@ -35,5 +36,15 @@ export class User {
   set(update: UserProps) {
     this.attributes.set(update);
     this.events.trigger("change");
+  }
+
+  fetch(): void {
+    const id = this.get("id");
+    if (typeof id !== "number") {
+      throw new Error("Cannot fetch without an id");
+    }
+    this.sync.fetch(id).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
   }
 }
