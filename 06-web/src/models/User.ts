f3ebeaf2956774456fc8874699305c7487e3612a
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-11-11 10:41:37 -0400

    Connecting Sync Back to User

--- 06-web/src/models/User.ts
+++ 06-web/src/models/User.ts
@@ -1,20 +1,19 @@
 import { Eventing } from "./Eventing";
+import { Sync } from "./Sync";
 
-// his implementation with interfaces
-// interface UserProps {
-//   name: string;
-//   age: number;
-// }
-
-// my try enforcing types
+// interface UserProps { name: string; age: number; }
+// using type instead of interface
 export type UserProps = {
   id?: number;
   name?: string;
   age?: number;
 };
 
+const http = "http://localhost:3000/users";
+
 export class User {
   public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(http);
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
