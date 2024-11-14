16af46c91911e2b7507f5e3773953b32c31d9c52
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-11-14 09:47:52 -0400

    Saving Data

--- 06-web/src/models/User.ts
+++ 06-web/src/models/User.ts
@@ -1,7 +1,7 @@
+import { AxiosResponse } from "axios";
 import { Eventing } from "./Eventing";
 import { Sync } from "./Sync";
 import { Attributes } from "./Attributes";
-import { AxiosResponse } from "axios";
 
 // interface UserProps { name: string; age: number; }
 // using type instead of interface
@@ -17,6 +17,7 @@ export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(http);
   public attributes: Attributes<UserProps>;
+
   constructor(attrs: UserProps) {
     this.attributes = new Attributes<UserProps>(attrs);
   }
@@ -26,25 +27,38 @@ export class User {
   }
 
   get trigger() {
-    return this.attributes.get;
+    return this.events.trigger;
   }
 
   get get() {
     return this.attributes.get;
   }
 
-  set(update: UserProps) {
+  set(update: UserProps): void {
     this.attributes.set(update);
     this.events.trigger("change");
   }
 
   fetch(): void {
     const id = this.get("id");
+
     if (typeof id !== "number") {
       throw new Error("Cannot fetch without an id");
     }
+
     this.sync.fetch(id).then((response: AxiosResponse): void => {
       this.set(response.data);
     });
+  }
+
+  save(): void {
+    this.sync
+      .save(this.attributes.getAll())
+      .then((response: AxiosResponse): void => {
+        this.trigger("save");
+      })
+      .catch(() => {
+        this.trigger("error");
+      });
   }
 }
