23835f23aa11c926815b818a3bd9e6ae94a06055
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-11-18 12:10:54 -0400

    Strict Null Checks

--- 06-web/src/index.ts
+++ 06-web/src/index.ts
@@ -2,6 +2,17 @@ import { UserForm } from "./views/UserForm";
 import { User } from "./models/User";
 
 const user = User.buildUser({ name: "NAME", age: 20 });
-const userForm = new UserForm(document.getElementById("root") as Element, user);
 
-userForm.render();
+// Prev way working code w/type assertion
+// const root = document.getElementById("root");
+// const userForm = new UserForm(root as Element, user);
+
+const root = document.getElementById("root");
+
+// with type guards
+if (root) {
+  const userForm = new UserForm(root, user);
+  userForm.render();
+} else {
+  throw new Error("Root element not found");
+}
